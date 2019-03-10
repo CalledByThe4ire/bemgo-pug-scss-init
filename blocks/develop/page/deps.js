218154c248656da891b054b63b14a2828064c3e9
Andrey Karamnoff <karamnoff@gmail.com>
2019-03-10 09:34:19 +0300

    initial commit

---
+++ blocks/develop/page/deps.js
@@ -0,0 +1,18 @@
+module.exports = {
+  nodes: [],
+
+  modules: [
+    {
+      from: 'node_modules/normalize.scss',
+      import: 'normalize.scss',
+    },
+    {
+      from: 'app/blocks/develop/page/base/',
+      import: 'fonts.scss',
+    },
+    {
+      from: 'app/blocks/develop/page/base/',
+      import: 'common.scss',
+    },
+  ],
+};
